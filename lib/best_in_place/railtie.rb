5ce9461de2799a43bd5a6e73c7fa40d8d0168a1f
Ville Sundberg <sundberg.ville@gmail.com>
2023-12-17 10:06:35 +0100

    Initialize with LookupContext

--- lib/best_in_place/railtie.rb
+++ lib/best_in_place/railtie.rb
@@ -4,7 +4,8 @@ require 'action_view/base'
 module BestInPlace
   class Railtie < ::Rails::Railtie #:nodoc:
     config.after_initialize do
-      BestInPlace::ViewHelpers = ActionView::Base.new({}, {}, "")
+      lookup_context = ActionView::LookupContext.new([])
+      BestInPlace::ViewHelpers = ActionView::Base.new(lookup_context, {}, nil)
     end
   end
 end
