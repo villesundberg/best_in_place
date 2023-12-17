4a39eae7417a4c6ef0c6476a3b4c491f12c575fb
Ville Sundberg <sundberg.ville@gmail.com>
2023-12-17 10:04:53 +0100

    Load helpers in Rails reloader

--- lib/best_in_place/engine.rb
+++ lib/best_in_place/engine.rb
@@ -1,8 +1,10 @@
 module BestInPlace
   class Engine < Rails::Engine
     initializer 'best_in_place' do
-      ActionView::Base.send(:include, BestInPlace::Helper)
-      ActionController::Base.send(:include, BestInPlace::ControllerExtensions)
+      Rails.application.reloader.to_prepare do
+        ActionView::Base.send(:include, BestInPlace::Helper)
+        ActionController::Base.send(:include, BestInPlace::ControllerExtensions)
+      end
     end
   end
 end
